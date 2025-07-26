cd8d2a31fb9e3634bd42f3dcb1fbe587813a3f39
Landing749 <arhanmeir.obrero@gmail.com>
2025-07-26 07:35:24 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,45 @@
+const CACHE_NAME = "yia-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/dashboard.html",
+  "/style.css",
+  "/login.js",
+  "/dashboard.js",
+  "/distress.js",
+  "/sheet.js",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
 
+// Install event
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activate event
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
